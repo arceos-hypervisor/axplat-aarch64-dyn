ca20dd3fc1c413e5df8eda8ec8a7b344e078e9f9
周睿 <zrufo747@outlook.com>
2025-11-06 10:00:22 +0800

    移除未使用的配置相关代码，简化构建脚本

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ use std::path::PathBuf;
 use serde::Deserialize;
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=AX_CONFIG_PATH");
-    let config_path =
-        std::env::var("AX_CONFIG_PATH").unwrap_or_else(|_| "axconfig.toml".to_string());
+    // println!("cargo:rerun-if-env-changed=AX_CONFIG_PATH");
+    // let config_path =
+        // std::env::var("AX_CONFIG_PATH").unwrap_or_else(|_| "axconfig.toml".to_string());
 
-    println!("cargo:rerun-if-changed={config_path}");
+    // println!("cargo:rerun-if-changed={config_path}");
 
     println!("cargo:rustc-link-search={}", out_dir().display());
     println!("cargo::rustc-link-arg=-Tlink.x");
@@ -19,11 +19,11 @@ fn main() {
     println!("cargo:rerun-if-changed={script}");
     let mut ld_content = std::fs::read_to_string(script).unwrap();
 
-    let config = std::fs::read_to_string(config_path).unwrap();
+    // let config = std::fs::read_to_string(config_path).unwrap();
 
-    let value: Config = toml::from_str(&config).unwrap();
+    // let value: Config = toml::from_str(&config).unwrap();
 
-    ld_content = ld_content.replace("{{SMP}}", &format!("{}", value.plat.cpu_num));
+    ld_content = ld_content.replace("{{SMP}}", &format!("{}", 16));
 
     std::fs::write(out_dir().join("link.x"), ld_content).expect("link.x write failed");
 }
@@ -32,13 +32,13 @@ fn out_dir() -> PathBuf {
     PathBuf::from(std::env::var("OUT_DIR").unwrap())
 }
 
-#[derive(Deserialize)]
-struct Config {
-    plat: Plat,
-}
+// #[derive(Deserialize)]
+// struct Config {
+//     plat: Plat,
+// }
 
-#[derive(Deserialize)]
-struct Plat {
-    #[serde(rename = "cpu-num")]
-    cpu_num: usize,
-}
+// #[derive(Deserialize)]
+// struct Plat {
+//     #[serde(rename = "cpu-num")]
+//     cpu_num: usize,
+// }
